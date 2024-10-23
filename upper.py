a39abe2e0049a05663c957366e6e6ee2ae401663
Abbas <AbbasHussainSyed@users.noreply.github.com>
2024-10-23 01:48:40 -0700

    upper.py

---
+++ upper.py
@@ -0,0 +1,22 @@
+# Initialize an empty list named 'suitcase'
+suitcase = []
+# TODO: Add items to your suitcase
+items_to_add = ["passport", "clothes", "haircare", "skincare", "gadgets", "accessories"]
+suitcase.extend(items_to_add)
+print(suitcase)
+
+# Declare a string variable for your goodbye message
+message = "goodbye! see you soon"
+# TODO: Assign a farewell message to the variable
+
+# Access the first and last item in your suitcase
+first = suitcase[0]
+last = suitcase[-1]
+# TODO: Print the first item in the suitcase
+print(first)
+# TODO: Print the last item in the suitcase
+print(last)
+# Send off with a farewell in uppercase
+goodbye = message.upper()
+# TODO: Print the farewell message in uppercase
+print(goodbye)
