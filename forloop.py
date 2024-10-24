8dac723f720f7054ad830958cfd439e4e0741a83
Abbas <AbbasHussainSyed@users.noreply.github.com>
2024-10-24 02:31:39 -0700

    forloop.py updated

--- forloop.py
+++ forloop.py
@@ -7,3 +7,11 @@ for fruit in fruits:
     # The wrong operator is used here, it should increase the count
     fruit_count += 1
 print(fruit_count)  # This should print the total number of fruits in the salad
+
+# List of fruits to be used in fruit salad
+fruits = ['apple', 'banana', 'kiwi', 'mango']
+
+# TODO: Loop over the list of fruits and add them to the fruit salad with a print statement
+for fruit in fruits:
+    # TODO: Print a statement for adding each fruit to the fruit salad.
+    print(f"added to fruit salad:", fruit)
