22e2e9d7a944cf5fc968b13bd7650306c92f7a3f
Abbas <AbbasHussainSyed@users.noreply.github.com>
2024-10-24 02:26:42 -0700

    whileloop.py

---
+++ whileloop.py
@@ -0,0 +1,9 @@
+# List of fruits to include in a fruit salad
+fruits = ['apple', 'banana', 'orange', 'kiwi', 'melon']
+
+i = 0;
+# Using a for loop to iterate over the list of fruits
+while i < len(fruits):
+        fruit = fruits[i]
+        print(f"Adding {fruit} to the salad.")
+        i +=1
