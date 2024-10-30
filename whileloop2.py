6ab66c6c462eb73619087b6a6a1594ef39eca034
Abbas <AbbasHussainSyed@users.noreply.github.com>
2024-10-30 00:19:07 -0700

    whileloop2.py updated

--- whileloop2.py
+++ whileloop2.py
@@ -5,11 +5,15 @@ fruits_in_salad = ""
 index = 0
 # TODO: Create a while loop that assembles a string of fruit names separated by spaces, without adding a space after the last fruit
 # Hint: Consider using a conditional to determine when to add a space
-while  index < len(fruits):
-        print(fruits[index], end=" ")
-        if index < len(fruits)-1:
-                print(" ", end=" ")
-        index += 1
+# iterates over the fruits list
+while index < len(fruits):
+    # end=" " modifies the print statement to print on the same line
+    print(fruits[index], end='')
+    # below condition checks if the index is the last fruit in the list
+    if index < len(fruits) - 1:
+        # after above it adds space to each word at the end
+        print(" ", end='')
+    index += 1
 
 """if fruits_in_salad.endswith(","):
                 result = " ".join(fruits_in_salad)
