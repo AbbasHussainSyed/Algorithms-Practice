fd63039e0934cd949208af355306777d3e1f7af4
Abbas <AbbasHussainSyed@users.noreply.github.com>
2024-11-27 19:56:29 -0800

    Check if the elements are sorted in increasing order

---
+++ CheckSort.py
@@ -0,0 +1,27 @@
+# Let's solve Question 4: Design a divide-and-conquer algorithm to determine if an array is sorted in increasing order.
+
+def is_sorted(arr):
+    if len(arr) == 1:
+        return arr
+
+    start = 0
+    end = len(arr)-1  # points to last position in array
+
+    mid = (start + end) // 2  # mid = "5"
+    print(mid)
+    for i in range(start, mid):
+        if arr[i] > arr[i+1]:
+            print(arr[i], arr[i+1])
+            return False
+
+    for i in range(mid, end):
+        if arr[i] > arr[i + 1]:
+            print(arr[i], arr[i+1])
+            return False
+    return True
+
+
+# Driver Program
+
+result = is_sorted([4, 5, 8, 7])
+print(result)
