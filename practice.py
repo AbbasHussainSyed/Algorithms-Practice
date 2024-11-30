28c9f2e6ad9241f75fb96b43d5657b1a3bd9f1b1
Abbas <AbbasHussainSyed@users.noreply.github.com>
2024-11-29 23:26:31 -0800

    O(n^3)

---
+++ practice.py
@@ -0,0 +1,12 @@
+def algorithm1(n):
+    total = 0
+    for i in range(n):
+        for j in range(n):
+            for k in range(j):
+                print(k)
+                total += 1
+    return total
+
+
+result = algorithm1(4)
+print(result)
