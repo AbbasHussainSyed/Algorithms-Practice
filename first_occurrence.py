d3e5fddb01b328a874dd536e887c57f802732919
Abbas <AbbasHussainSyed@users.noreply.github.com>
2024-12-08 00:50:11 -0800

    Auto-commit: Sun Dec  8 00:50:11 PST 2024

--- first_occurrence.py
+++ first_occurrence.py
@@ -1,5 +1,5 @@
 #  modified binary search algorithm to find the first index in the array
 def first_occurrence(arr, val, low, high):
-    
+    #  base case
     if low > high:
         return "value not in array"
