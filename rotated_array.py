875dea9ce62717dea7bb1fd1630bdcd16aa5fe2c
Abbas <AbbasHussainSyed@users.noreply.github.com>
2024-12-08 01:33:43 -0800

    Auto-commit: Sun Dec  8 01:33:43 PST 2024

--- rotated_array.py
+++ rotated_array.py
@@ -4,3 +4,8 @@
 # Input array: [6, 7, 1, 2, 3, 4, 5]
 # Target: 2
 # Expected output: 3 (because 2 is at index 3)
+
+def rotated_array(arr, target, low, high):
+    if low > high:
+        return -1
+    
