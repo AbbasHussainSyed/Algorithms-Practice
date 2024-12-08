2a5aa34391e3d921d9f317a95b7d9c36c0189ce3
Abbas <AbbasHussainSyed@users.noreply.github.com>
2024-12-08 01:45:16 -0800

    Auto-commit: Sun Dec  8 01:45:16 PST 2024

--- rotated_array.py
+++ rotated_array.py
@@ -14,4 +14,6 @@ def rotated_array(arr, target, low, high):
     if arr[mid] == target:
         return mid
     elif arr[mid] > target and arr[mid]-arr[mid-1] == 1:
-        return rotated_array(arr, target, low, mid)
+        return rotated_array(arr, target, low, mid-1)
+    else:
+        return
