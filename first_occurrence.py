1ac95aa90cf968a343788e37578687f68ba49e55
Abbas <AbbasHussainSyed@users.noreply.github.com>
2024-12-08 00:56:14 -0800

    Auto-commit: Sun Dec  8 00:56:14 PST 2024

--- first_occurrence.py
+++ first_occurrence.py
@@ -5,4 +5,9 @@ def first_occurrence(arr, val, low, high):
         return "value not in array"
 
     mid = low + (high-low) // 2
-    
+
+    if arr[mid] == val:
+        if arr[mid] == 0 or arr[mid-1] < val:
+            return arr[mid]
+        else:
+            return 
