2d584a077afb1df2e7acbb5c9653202e79fd5b38
Abbas <AbbasHussainSyed@users.noreply.github.com>
2024-12-08 01:16:02 -0800

    Auto-commit: Sun Dec  8 01:16:02 PST 2024

--- count_occurrence.py
+++ count_occurrence.py
@@ -4,4 +4,4 @@ import  last_occurrence
 def count_occurrence(arr, val):
 
     first = first_occurrence(arr, val, low, high)
-    last = last_occurrence(arr, val, low, )
+    last = last_occurrence(arr, val, low, high)
