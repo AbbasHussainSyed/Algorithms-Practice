5b8127cd379034dcd93531b95275b92a0bc71f09
Abbas <AbbasHussainSyed@users.noreply.github.com>
2024-12-08 01:35:13 -0800

    Auto-commit: Sun Dec  8 01:35:13 PST 2024

--- rotated_array.py
+++ rotated_array.py
@@ -1,8 +1,8 @@
 # Find the index of target value in the rotated sorted array
 # Return -1 if the target is not found
 # rotated sorted array
-# Input array: [6, 7, 1, , 3, 4, 5]
-# Target: 2
+# Input array: [6, 7, 1, 2, 3, 4, 5]
+# Target: 1
 # Expected output: 3 (because 2 is at index 3)
 
 def rotated_array(arr, target, low, high):
