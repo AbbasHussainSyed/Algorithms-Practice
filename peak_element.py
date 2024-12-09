60535710de021fa51aed588d02165acc0af89047
Abbas <AbbasHussainSyed@users.noreply.github.com>
2024-12-08 20:44:52 -0800

    Auto-commit: Sun Dec  8 20:44:52 PST 2024

--- peak_element.py
+++ peak_element.py
@@ -1,1 +1,14 @@
+# For array [1, 3, 20, 4, 1, 0]
+# 20 is a peak element because it's greater than both 3 and 4
+# Should return index 2
 
+# For array [5, 10, 20, 15]
+# 20 is a peak because it's greater than 10 and 15
+# Should return index 2
+
+# Note: If there are multiple peaks, we can return any one of them
+
+def peak_element(arr, peak, low, high):
+
+    if len(arr) == 1:
+        return 0
