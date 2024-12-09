8d901d4af5781e3e26a37c9a1dfef2663c1e9e2b
Abbas <AbbasHussainSyed@users.noreply.github.com>
2024-12-08 17:52:48 -0800

    Auto-commit: Sun Dec  8 17:52:48 PST 2024

--- exponential_search.py
+++ exponential_search.py
@@ -1,6 +1,6 @@
 # Problem: Implement exponential search to find target element in a sorted array
 # Example array: [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
-# If target = 12, return its index (5)
+# If target = 14, return its index (6)
 # If target not found, return -1
 
 def exponential_search(arr, target, low, high):
@@ -11,5 +11,7 @@ def exponential_search(arr, target, low, high):
     if arr[mid] == target:
         return mid
 
-    if arr[mid]*2 == arr[mid+1]:
-        return
+    if arr[mid]<target:
+        if arr[mid]*2 == arr[mid+1]:
+            return exponential_search(arr, target, mid+1, high)
+        
