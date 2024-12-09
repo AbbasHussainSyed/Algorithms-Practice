5deb67da1d14835befe7fddb0e06bd457e606add
Abbas <AbbasHussainSyed@users.noreply.github.com>
2024-12-08 17:53:22 -0800

    Auto-commit: Sun Dec  8 17:53:22 PST 2024

--- exponential_search.py
+++ exponential_search.py
@@ -16,3 +16,5 @@ def exponential_search(arr, target, low, high):
             return exponential_search(arr, target, mid+1, high)
         else:
             return exponential_search(arr, target, low, mid-1)
+
+    
