8fbe74b9cba2abc658754a8868485f6dd45fb4bd
Abbas <AbbasHussainSyed@users.noreply.github.com>
2024-12-08 18:04:27 -0800

    Auto-commit: Sun Dec  8 18:04:27 PST 2024

--- exponential_search.py
+++ exponential_search.py
@@ -10,4 +10,5 @@ def binary_search(arr, target, low, high):
     mid = low + (high - low) // 2
     if arr[mid] == target:
      return mid
-    
+    elif arr[mid] > target:
+        return binary_search(arr, target, low,)
