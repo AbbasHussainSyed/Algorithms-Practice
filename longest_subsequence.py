251b99fd18e32cf428b4cb8eb71ed2e0adca9f5b
Abbas <AbbasHussainSyed@users.noreply.github.com>
2024-12-10 14:16:10 -0800

    Auto-commit: Tue Dec 10 14:16:10 PST 2024

--- longest_subsequence.py
+++ longest_subsequence.py
@@ -2,8 +2,8 @@
 # example arr = [3,10,2,1,20] the longest subsequence is 3
 
 def longest_subsequence(arr):
-    M = [1]*len(arr)
+    M = [1] * len(arr)
     print(M)
 
-result = longest_subsequence([3,10,2,1,20])
 
+result = longest_subsequence([3, 10, 2, 1, 20])
