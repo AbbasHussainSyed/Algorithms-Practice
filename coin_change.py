11e5b7a9b1935b1098a05c75996c386525d08e1e
Abbas <AbbasHussainSyed@users.noreply.github.com>
2024-12-09 21:44:13 -0800

    Auto-commit: Mon Dec  9 21:44:13 PST 2024

--- coin_change.py
+++ coin_change.py
@@ -1,12 +1,1 @@
-# dynamic programming algorithm for coins change
 
-def coins_change(coin):
-    M = [0, 1, 2, 3, 4, 1, 2, 1]
-
-    for i in range(8, coin):
-        M[i] = min(M[i - 1] + 1, M[i - 5] + 1, M[i - 7] + 1)
-    return M[coin]
-
-
-result = coins_change(11)
-print(result)
