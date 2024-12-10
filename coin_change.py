b220c87f1a4deeb68e9db830d41e0efca8ada76b
Abbas <AbbasHussainSyed@users.noreply.github.com>
2024-12-09 21:44:29 -0800

    Auto-commit: Mon Dec  9 21:44:29 PST 2024

--- coin_change.py
+++ coin_change.py
@@ -3,7 +3,9 @@
 def coins_change(coin):
     M = [0, 1, 2, 3, 4, 1, 2, 1]
 
-    for i in range(8, coin):
+    M.extend()
+
+    for i in range(8, coin+1):
         M[i] = min(M[i - 1] + 1, M[i - 5] + 1, M[i - 7] + 1)
     return M[coin]
 
