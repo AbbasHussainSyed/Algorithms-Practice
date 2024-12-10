4fda4cea99dc736f5f9ebb3cba664a3a2061320d
Abbas <AbbasHussainSyed@users.noreply.github.com>
2024-12-10 15:52:01 -0800

    Auto-commit: Tue Dec 10 15:52:01 PST 2024

--- div&conq_find_mean.py
+++ div&conq_find_mean.py
@@ -9,4 +9,4 @@ def find_mean(arr):
     mid = len(arr) // 2
 
     left_mean, left_count = find_mean(arr[:mid])
-    right_mean, right_count = right_mean(arr[mid:])
+    right_mean, right_count = find_mean(arr[mid:])
