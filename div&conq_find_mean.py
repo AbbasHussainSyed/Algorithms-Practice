49736150645675b8bb00196b7ec30a379267be2e
Abbas <AbbasHussainSyed@users.noreply.github.com>
2024-12-10 15:58:05 -0800

    Auto-commit: Tue Dec 10 15:58:05 PST 2024

--- div&conq_find_mean.py
+++ div&conq_find_mean.py
@@ -4,7 +4,7 @@ def find_mean(arr):
     if len(arr) == 1:
         return arr[0], 1
     if len(arr) == 2:
-        return arr[0] + arr[1] // 2, 2
+        return (arr[0] + arr[1]) // 2, 2
 
     mid = len(arr) // 2
 
