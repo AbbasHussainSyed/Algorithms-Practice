f3825345a727a9c91dcc65305797e0c2b6b36092
Abbas <AbbasHussainSyed@users.noreply.github.com>
2024-12-09 21:27:42 -0800

    Auto-commit: Mon Dec  9 21:27:42 PST 2024

--- tribonacci_dp.py
+++ tribonacci_dp.py
@@ -1,13 +1,13 @@
 # dynamic programming tribonacci algorithm
 
 def tribonacci(n):
+    M = [0, 0, 1]
 
-    M = [0,0,1]
-
-    for i in range(3, n+1):
-        M.append(M[i-1]+M[i-2]+M[i-3])
+    for i in range(3, n + 1):
+        M.append(M[i - 1] + M[i - 2] + M[i - 3])
 
     return M[n]
+
 
 result = tribonacci(5)
 print(result)
