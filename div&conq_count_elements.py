9d1b2cc6c826246694f2331f847ffa1a00d6fcc7
Abbas <AbbasHussainSyed@users.noreply.github.com>
2024-12-10 15:02:40 -0800

    Auto-commit: Tue Dec 10 15:02:40 PST 2024

--- div&conq_count_elements.py
+++ div&conq_count_elements.py
@@ -1,2 +1,6 @@
 # Given an array A of n integers and a value x, count how many elements in A are strictly greater than x.
-# Use a divide-and-conquer algorithm to solve the p
+# Use a divide-and-conquer algorithm to solve the problem
+
+def count_elements(arr, x):
+    if len(arr)==0:
+        return 0
