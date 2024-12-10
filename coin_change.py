faa87fc23c88acb4051033563ef484624ba3a46a
Abbas <AbbasHussainSyed@users.noreply.github.com>
2024-12-09 21:47:08 -0800

    Auto-commit: Mon Dec  9 21:47:08 PST 2024

--- coin_change.py
+++ coin_change.py
@@ -1,11 +1,11 @@
 # dynamic programming algorithm for coins change of 1, 5, 7
 
 def coins_change(coin):
-    M = [0, 1, 2, 3, 4, 1, 2, 1] # base case coins needed for each amount till 7
+    M = [0, 1, 2, 3, 4, 1, 2, 1]  # base case coins needed for each amount till 7
 
-    M.extend([float('inf')]*(coin-7))
+    M.extend([float('inf')] * (coin - 7))
 
-    for i in range(8, coin+1):
+    for i in range(8, coin + 1):
         M[i] = min(M[i - 1] + 1, M[i - 5] + 1, M[i - 7] + 1)
     return M[coin]
 
